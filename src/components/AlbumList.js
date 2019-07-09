7cd726110ce584da439a48d64329ae7c0ccbb64b
yasuhiro <yasucom4646@gmail.com>
2019-07-09 12:16:28 -0700

    Deleted View, Text, Image,

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'; 
-import { View, Text, Image, ScrollView } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
