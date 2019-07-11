d4aef378286a53d45f7834e7a34d013863b4a5e7
yasuhiro <yasucom4646@gmail.com>
2019-07-11 00:02:29 -0700

    Added View, Text, Image, Linking and creted style

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,27 +1,71 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+    const { title, artist, thumbnail_image, image, url } = album;
+    const { 
+        thumbnailStyle, 
+        headerContentStyle,
+        thumbnailContainerStyle,
+        headerTextStyle,
+        imageStyle
+     } = styles;
     return (
         <Card>
             <CardSection>
-                <Image
-                        style={{width: 50, height: 50}}
-                        source={{uri: props.album.thumbnail_image}}
+                <View style={thumbnailContainerStyle}>
+                    <Image
+                        style={thumbnailStyle}
+                        source={{uri: thumbnail_image}}
                         />
-                <Text>{props.album.title}</Text>
-                <Text>{props.album.artist}</Text>
+                </View>
+                <View style={headerContentStyle}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>   
             </CardSection>
             <CardSection>
                 <Image
-                        style={{width: 300, height: 300}}
-                        source={{uri: props.album.image}}
+                        style={imageStyle}
+                        source={{uri: image}}
                         />
+            </CardSection>
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url) }>
+                    Buy now!
+                </Button>
             </CardSection>
         </Card>
     );
+}
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18,
+    },
+    thumbnailStyle: {
+        width: 50,
+        height: 50,
+        marginRight: 10,
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    imageStyle: {
+        width: null,
+        height: 300,
+        flex: 1
+    }
 }
 
 export default AlbumDetail;
